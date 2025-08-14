2f9c94209c75cf968102aa15b9decfae1b28bdce
Christian Bock <info@elektro-bock.com>
2025-08-14 22:42:39 +0200

    menu session added

--- src/app/app/home/home.component.ts
+++ src/app/app/home/home.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { HeroComponent } from "../hero/hero.component";
+import { MenueComponent } from "../menue/menue.component";
+import { WhyMeComponent } from "../why-me/why-me.component";
 
 @Component({
   selector: 'app-home',
-  imports: [HeroComponent],
+  imports: [HeroComponent, MenueComponent, WhyMeComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
