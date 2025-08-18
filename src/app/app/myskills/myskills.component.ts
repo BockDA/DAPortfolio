9238fb489e58e0ee5ee4d1ca9395f7eef837e406
Christian Bock <info@elektro-bock.com>
2025-08-18 12:35:44 +0200

    create section myskills

---
+++ src/app/app/myskills/myskills.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-myskills',
+  imports: [],
+  templateUrl: './myskills.component.html',
+  styleUrl: './myskills.component.scss'
+})
+export class MyskillsComponent {
+
+}
