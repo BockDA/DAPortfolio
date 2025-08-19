c15aa02accfe4e5355ae0dfd3620789258b7fe02
Christian Bock <info@elektro-bock.com>
2025-08-19 13:07:58 +0200

    myprojects starting framework created

--- src/app/app/home/home.component.ts
+++ src/app/app/home/home.component.ts
@@ -4,10 +4,17 @@ import { MenueComponent } from "../menue/menue.component";
 import { WhyMeComponent } from "../why-me/why-me.component";
 import { LogoComponent } from '../logo/logo.component';
 import { MyskillsComponent } from "../myskills/myskills.component";
+import { MyprojectsComponent } from '../myprojects/myprojects.component';
+
 
 @Component({
   selector: 'app-home',
-  imports: [HeroComponent, MenueComponent, WhyMeComponent, MyskillsComponent],
+  imports: [HeroComponent, 
+    MenueComponent, 
+    WhyMeComponent,
+     MyskillsComponent,
+     MyprojectsComponent
+    ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
--- src/app/app/myprojects/myprojects.component.ts
+++ src/app/app/myprojects/myprojects.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-myprojects',
-  imports: [],
+  imports: [TranslateModule],
   templateUrl: './myprojects.component.html',
   styleUrl: './myprojects.component.scss'
 })
