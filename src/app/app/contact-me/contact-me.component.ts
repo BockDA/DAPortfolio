f96dade301961de7031b8811ca373f4e95bff7ef
Bockinger <cbock2011@gmail.com>
2025-08-25 22:31:38 +0200

    contact form created

--- src/app/app/contact-me/contact-me.component.ts
+++ src/app/app/contact-me/contact-me.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-contact-me',
-  imports: [TranslateModule],
+  imports: [TranslateModule,FormsModule],
   templateUrl: './contact-me.component.html',
   styleUrl: './contact-me.component.scss'
 })
 export class ContactMeComponent {
+
+
+  contactData = {
+    name: "",
+    email: "",
+    message:""
+  }
+
+  onSubmit(ngForm: NgForm) {
+    if (ngForm.valid && ngForm.submitted) {
+
+      console.log(this.contactData);
+    }
+  }
 
 }
