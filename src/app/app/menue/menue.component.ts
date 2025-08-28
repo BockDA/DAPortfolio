67a91138914c3fe5e3c16f1f21be61af2c3f4802
Bockinger <cbock2011@gmail.com>
2025-08-28 23:25:41 +0200

    menu component skill added

--- src/app/app/menue/menue.component.ts
+++ src/app/app/menue/menue.component.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
-import { LogoComponent } from "../logo/logo.component";
+import { LogoComponent } from '../logo/logo.component';
 import { TranslateModule } from '@ngx-translate/core';
-
 
 @Component({
   selector: 'app-menue',
-  imports: [LogoComponent,TranslateModule],
+  imports: [LogoComponent, TranslateModule],
   templateUrl: './menue.component.html',
-  styleUrl: './menue.component.scss'
+  styleUrl: './menue.component.scss',
 })
 export class MenueComponent {
+  menuAktiv: string = '';
 
+  setMenuAktiv(section: string) {
+    console.log('tsdgdssg');
+    this.menuAktiv = section;
+  }
 }
