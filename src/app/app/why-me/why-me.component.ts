b9d6cf4f4a690a3ab9fa6a91d49f3725bbc18a63
Bockinger <cbock2011@gmail.com>
2025-09-09 20:18:09 +0200

    why me component revised

--- src/app/app/why-me/why-me.component.ts
+++ src/app/app/why-me/why-me.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 import { LogoComponent } from '../logo/logo.component';
 import { TranslateModule } from '@ngx-translate/core';
+import { MyFunctionsService } from '../../services/my-functions.service';
 
 
 @Component({
   selector: 'app-why-me',
   imports: [TranslateModule],
   templateUrl: './why-me.component.html',
-  styleUrl: './why-me.component.scss'
+  styleUrl: './why-me.component.scss',
 })
 export class WhyMeComponent {
-
+  constructor(public setAktiv: MyFunctionsService) {}
 }
