fa34554d744520677b771438537944ac90998667
Bockinger <cbock2011@gmail.com>
2025-09-30 23:12:51 +0200

    responsive APP menue V1

--- src/app/hero/hero.component.ts
+++ src/app/hero/hero.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { LogoComponent } from '../logo/logo.component';
 import { TranslateModule } from '@ngx-translate/core';
 import { MyFunctionsService } from '../../services/my-functions.service';
+import { MenueComponent } from "../menue/menue.component";
 
 @Component({
   selector: 'app-hero',
-  imports: [LogoComponent, TranslateModule],
+  imports: [LogoComponent, TranslateModule, MenueComponent],
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.scss',
 })
