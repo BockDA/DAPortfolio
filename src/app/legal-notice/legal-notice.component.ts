410373f777811ff1cad24bbffb03c480d04690c2
Bockinger <cbock2011@gmail.com>
2025-10-20 20:44:52 +0200

    legal notice V8

--- src/app/legal-notice/legal-notice.component.ts
+++ src/app/legal-notice/legal-notice.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { MenueComponent } from '../menue/menue.component';
 import { TranslateModule } from '@ngx-translate/core';
 import { NgFor } from '@angular/common';
+import { FooterComponent } from "../footer/footer.component";
 
 @Component({
   selector: 'app-legal-notice',
-  imports: [MenueComponent, TranslateModule, NgFor],
+  imports: [MenueComponent, TranslateModule, NgFor, FooterComponent],
   templateUrl: './legal-notice.component.html',
   styleUrl: './legal-notice.component.scss',
 })
