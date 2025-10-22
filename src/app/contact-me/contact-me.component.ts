bbb0c0247f31b97035554d0e9e04fb45d531c6dd
Bockinger <cbock2011@gmail.com>
2025-10-22 23:51:41 +0200

    sendmail V2

--- src/app/contact-me/contact-me.component.ts
+++ src/app/contact-me/contact-me.component.ts
@@ -1,10 +1,9 @@
-import { Component,inject } from '@angular/core';
+import { Component } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
 import { TranslateModule } from '@ngx-translate/core';
 import { MenueComponent } from '../menue/menue.component';
 import { HeroComponent } from '../hero/hero.component';
 import { MyFunctionsService } from '../../services/my-functions.service';
-import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-contact-me',
@@ -13,9 +12,10 @@ import { HttpClient } from '@angular/common/http';
   styleUrl: './contact-me.component.scss',
 })
 export class ContactMeComponent {
+//contactForm: NgForm;
+ 
+  
   constructor(public setAktiv: MyFunctionsService) { }
-
-http=inject(HttpClient);
 
   contactData = {
     name: '',
@@ -23,35 +23,9 @@ http=inject(HttpClient);
     message: '',
   };
 
-  mailTest = true;
-
-  post = {
-    endPoint: 'https://deineDomain.de/sendMail.php',
-    body: (payload: any) => JSON.stringify(payload),
-    options: {
-      headers: {
-        'Content-Type': 'text/plain',
-        responseType: 'text',
-      },
-    },
-  };
-
   onSubmit(ngForm: NgForm) {
-    if (ngForm.submitted && ngForm.form.valid && !this.mailTest) {
-      this.http.post(this.post.endPoint, this.post.body(this.contactData))
-        .subscribe({
-          next: (response) => {
-
-            ngForm.resetForm();
-          },
-          error: (error) => {
-            console.error(error);
-          },
-          complete: () => console.info('send post complete'),
-        });
-    } else if (ngForm.submitted && ngForm.form.valid && this.mailTest) {
-
-      ngForm.resetForm();
+    if (ngForm.valid && ngForm.submitted) {
+      console.log(this.contactData);
     }
   }
 }
--- src/app/legal-notice/legal-notice.component.ts
+++ src/app/legal-notice/legal-notice.component.ts
@@ -1,14 +1,21 @@
 import { Component } from '@angular/core';
 import { MenueComponent } from '../menue/menue.component';
 import { TranslateModule } from '@ngx-translate/core';
-import { NgFor } from '@angular/common';
-import { FooterComponent } from "../footer/footer.component";
+import { CommonModule, NgFor, NgIf } from '@angular/common';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-legal-notice',
-  imports: [MenueComponent, TranslateModule, NgFor, FooterComponent],
+  imports: [
+    MenueComponent,
+    TranslateModule,
+    CommonModule,
+    NgFor,
+    NgIf,
+    FooterComponent,
+  ],
   templateUrl: './legal-notice.component.html',
-  styleUrl: './legal-notice.component.scss',
+  styleUrls: ['./legal-notice.component.scss'],
 })
 export class LegalNoticeComponent {
   numbers = Array(5).fill(0);
