0b279fc2186af3d2cd2cf06d85186fee4bad8fb7
Bockinger <cbock2011@gmail.com>
2025-10-22 21:56:27 +0200

    sendmail V1

--- src/app/contact-me/contact-me.component.ts
+++ src/app/contact-me/contact-me.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component,inject } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
 import { TranslateModule } from '@ngx-translate/core';
 import { MenueComponent } from '../menue/menue.component';
 import { HeroComponent } from '../hero/hero.component';
 import { MyFunctionsService } from '../../services/my-functions.service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-contact-me',
@@ -12,7 +13,9 @@ import { MyFunctionsService } from '../../services/my-functions.service';
   styleUrl: './contact-me.component.scss',
 })
 export class ContactMeComponent {
-  constructor(public setAktiv: MyFunctionsService) {}
+  constructor(public setAktiv: MyFunctionsService) { }
+
+http=inject(HttpClient);
 
   contactData = {
     name: '',
@@ -20,9 +23,35 @@ export class ContactMeComponent {
     message: '',
   };
 
+  mailTest = true;
+
+  post = {
+    endPoint: 'https://deineDomain.de/sendMail.php',
+    body: (payload: any) => JSON.stringify(payload),
+    options: {
+      headers: {
+        'Content-Type': 'text/plain',
+        responseType: 'text',
+      },
+    },
+  };
+
   onSubmit(ngForm: NgForm) {
-    if (ngForm.valid && ngForm.submitted) {
-      console.log(this.contactData);
+    if (ngForm.submitted && ngForm.form.valid && !this.mailTest) {
+      this.http.post(this.post.endPoint, this.post.body(this.contactData))
+        .subscribe({
+          next: (response) => {
+
+            ngForm.resetForm();
+          },
+          error: (error) => {
+            console.error(error);
+          },
+          complete: () => console.info('send post complete'),
+        });
+    } else if (ngForm.submitted && ngForm.form.valid && this.mailTest) {
+
+      ngForm.resetForm();
     }
   }
 }
