04ba7efd3031b62a4db5ff414d1bbad90794ccdb
Bockinger <cbock2011@gmail.com>
2025-11-11 22:41:06 +0100

    feedback 3 revised revised V4

--- src/app/tab-teamplayer/tab-teamplayer.component.ts
+++ src/app/tab-teamplayer/tab-teamplayer.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
 import { NgFor } from '@angular/common';
+
 @Component({
   selector: 'app-tab-teamplayer',
-  imports: [TranslateModule,NgFor],
+  imports: [TranslateModule, NgFor],
   templateUrl: './tab-teamplayer.component.html',
   styleUrl: './tab-teamplayer.component.scss'
 })
